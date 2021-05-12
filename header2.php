4b27a826a3f8c1c79c83c5fa18221f1ced89e11a
EnriqueMolin4 <eduardo.molina.isc@gmail.com>
2021-05-12 15:26:09 -0500

    header

---
+++ header2.php
@@ -0,0 +1,26 @@
+<?php  include('modelos/loginredirect.php');?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Responsive sidebar template with sliding effect and dropdown menu based on bootstrap 3">
+    <title>Sinttecom_SAE</title>
+
+    <!-- using online links -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS"
+        crossorigin="anonymous">
+        <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/bs4/jq-3.3.1/dt-1.10.18/b-1.5.2/b-html5-1.5.2/datatables.min.css"/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.css">
+        <link rel="stylesheet" href="css/jquery-ui.min.css">
+    <link rel="stylesheet" href="css/jquery.datetimepicker.min.css">
+    <link rel="stylesheet" href="//malihu.github.io/custom-scrollbar/jquery.mCustomScrollbar.min.css">
+    <link rel="stylesheet" href="css/main.css">
+    <!--<link rel="stylesheet" href="css/sidebar-themes.css">-->
+     <link rel="stylesheet" href="css/zoomifyc.min.css">
+    <link rel="shortcut icon" type="image/png" href="img/favicon.png" />
+    <!--<link rel="stylesheet" href="css/modern.min.css">-->
+    <link rel="stylesheet" href="css/nuevoEstilo.css">
+    <link rel="stylesheet" href="css/estiloMenu.css">
+</head>
