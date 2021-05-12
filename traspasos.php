a2976cd25d69e12a8ee345c49420207e38c10a7f
EnriqueMolin4 <eduardo.molina.isc@gmail.com>
2021-05-12 16:02:36 -0500

    traspasos

---
+++ traspasos.php
@@ -0,0 +1,167 @@
+<?php require("header.php"); ?>
+
+<body>
+<div class="page-wrapper ice-theme sidebar-bg bg1 toggled">
+            <a id="show-sidebar" class="btn btn-sm btn-dark" href="#">
+                    <i class="fas fa-bars"></i>
+                  </a>
+        <nav id="sidebar" class="sidebar-wrapper">
+            <?php include("menu.php"); ?>
+        </nav>
+        <!-- page-content  -->
+        <main class="page-content pt-2">
+            <div id="overlay" class="overlay">
+           
+            </div>
+            <div class="page-title">
+               <h3>TRASPASOS</h3> 
+            </div><br>
+            <div class="row">
+            <?php if( searchMenuEdit($_SESSION['Modules'],'url','traspasos') == '1') { ?>
+                <div class="col-sm-6">
+                    <label for="excelMasivo" class="col-form-label-sm">CREACION TRASPASOS MASIVOS</label> 
+                    <input class="input-file" type="file" id="excelMasivoTraspasos" name="excelMasivoAsignacion">
+                    <button class="btn btn-success btn-sm" id="btnCargarExcelTraspasos">CARGAR</button>
+                </div>
+            <?php } ?>
+            </div><br>
+
+
+            <div class="container-fluid p-1 panel-white">
+            
+            <br>
+            <h5>BUSQUEDA</h5>
+                <div class="row  mb-4"> 
+                    <div class="col">
+                        <label for="almacen" class="col-form-label-sm">ALMACENES</label>
+                        <select id="almacen" name="almacen" class="form-control form-control-sm searchInventario">
+                                <option value="0" selected>Seleccionar</option>
+                        </select>
+                    </div>   
+                    <div class="col">
+                        <label for="tecnico" class="col-form-label-sm">TECNICO</label>
+                        <select id="tecnico" name="tecnico" class="form-control form-control-sm searchInventario">
+                                <option value="0" selected>Seleccionar</option>
+                        </select>
+                    </div>   
+                </div>
+
+                <div class="table-responsive">
+                    
+                
+                    <table id="traspasos"  class="table table-bordered table-responsive">
+                        <thead>
+                            <tr>
+                                <th width="200px">NO GUIA</th>
+                                <th width="2500px">C RASTREOS</th>
+                                <th width="250px">ORIGEN</th>
+                                <th width="300px">DESTINO</th>
+                                <th width="250px">ESTATUS</th>
+                                <th width="250px">FECHA</th>
+                                <th>ACCION</th>
+                            </tr>
+                        </thead>
+                        <tbody> 
+                    
+                        </tbody>
+                    </table>
+
+                </div>
+                <br />
+                <input type="hidden" id="no_guia" name="no_guia" value="0">
+				<input type="hidden" id="userPerm" value="<?php echo isset($_SESSION['tipo_user']) ? $_SESSION['tipo_user'] : 0 ; ?>">
+            </div>
+        </main>
+        <!-- page-content" -->
+        
+    </div>
+    <div class="modal fade" id="showAvisosCargas" role="dialog">
+        <div class="modal-dialog">
+            <!-- Modal content-->
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                </div>
+                <div class="modal-body" >
+                    <div style="overflow-y: scroll; height:400px;" id="bodyCargas">
+                </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="submit" class="btn btn-default btn-default pull-left" data-dismiss="modal"><span class="glyphicon glyphicon-remove"></span> Cancel</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Modal Detalle Traspaso -->
+    <div class="modal fade" tabindex="-1" role="dialog" id="showDetalle" data-backdrop="static" data-keyboard="false">
+        <div class="modal-dialog mw-100 w-75" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">TRASPASOS</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-mb-6 p-2"><p class="text-uppercase">NO SERIE: <span class="font-weight-bold" id="noGuia_det"></span></p></div>
+                            <div class="col-mb-4 p-2"><p class="text-uppercase">CODIGO RASTREO: <span class="font-weight-bold" id="codigorastreo_det"></span></p></div>
+                        </div>
+                        <div class="row">
+                            <div class="col-mb-6 p-2"><p class="text-uppercase ">ORIGEN: <span class="font-weight-bold" id="origen_det"></span></p></div>
+                            <div class="col-mb-4 p-2"><p class="text-uppercase">DESTINO: <span class="font-weight-bold" id="destino_det"></span></div>
+                        </div>
+                    </div> 
+                    <div class="row">
+                        <div class="table-responsive">
+                            <table id="traspasositems"  class="table" style="width:100%">
+                                <thead>
+                                    <tr>
+                                        <th width="300px">TIPO</th>
+                                        <th width="300px">NO SERIE</th>
+                                        <th width="300px">INSUMO</th>
+                                        <th width="300px">CANTIDAD</th>
+                                        <th width="300px">NOTAS</th>
+                                        <th width="300px">ACEPTADA</th>
+                                        <th width="300px">FECHA ACTUALIZACION</th>
+                                        <th width="300px">ACCION</th>
+                                    </tr>
+                                </thead>
+                                <tbody> 
+                                
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>  
+                    
+                </div>
+                <div class="modal-footer">
+                    <input type="hidden" value="0" id="noGuia" name="noGuia">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal" id="btnCancel" name="brnCancel">Cerrar</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- page-wrapper -->
+
+    <!-- using online scripts -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut"
+        crossorigin="anonymous"></script>
+        <script type="text/javascript" src="https://cdn.datatables.net/v/bs4/jq-3.3.1/dt-1.10.18/b-1.5.2/b-html5-1.5.2/datatables.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k"
+        crossorigin="anonymous"></script>
+    <script src="//malihu.github.io/custom-scrollbar/jquery.mCustomScrollbar.concat.min.js"></script>
+    <script src="js/jquery-ui.min.js"></script>
+    <script src="js/moment-with-locales.js"></script>
+    <script src="js/Chart.bundle.min.js"></script>
+    <script type="text/javascript" src="js/jquery.datetimepicker.full.min.js"></script>
+    <script type="text/javascript" src="js/jquery.toaster.js"></script>
+    <script type="text/javascript" src="js/jquery.validate.min.js"></script> 
+    <script src="js/main.js"></script>
+    <script type="text/javascript" src="js/traspasos.js"></script> 
+</body>
+
+</html>
