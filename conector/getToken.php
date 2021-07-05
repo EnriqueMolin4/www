17cd4b93af8a14f637ce8315e8831205e0768ac3
EnriqueMolin4 <eduardo.molina.isc@gmail.com>
2021-07-05 11:51:03 -0500

    Coments to cURL

--- conector/getToken.php
+++ conector/getToken.php
@@ -17,7 +17,7 @@ curl_setopt_array($curl, array(
 	"grant_type": "client_credentials",
 	"audience": "https://sgse.microformas.com.mx/"
 }',// todo los datos para enviar vía HTTP "POST"
-  CURLOPT_HTTPHEADER => array(//Un array de campos 
+  CURLOPT_HTTPHEADER => array(//Un array de campos a configurar para el header HTTP
     'Content-Type: application/json',
     'Cookie: .AspNetCore.Identity.Application=CfDJ8GVzBE9KDpBAposh7P86ebdLXSTU5yDkQ4u9OpWpYW2DufRpE8mRI2UYzKfvxL5crsBUrQQmmuiokYa0zzqU9ltSddNUvVnm9Do23xYTeK-3bvenOpDokAXVuAxC0_8wcdY9UWMigV4OY8mEMw3VY_WCgmPZZfw7wq5rLLbRJZnwXYEh7682gg1NHRwNEEAA4X6IpULJsLnAtbXzFhnyXibwkRpUEqIP0wMBle0hS9q64Lkqorf9d44KvrMks4KfxZ5PbsJkhqf10UXzyBZ58Rirldh_M3xPbUJxIag2o5uKyCwkJOtbis26Xpf94lo2eQiiHc_tJ-gIkRuoRsp6cyiD_5jXkW-vzyW0P_EnAJPkezJxgnU1Yy53eT9imbyDKpHS6s6yjnJsI4BJeaXOi__OlDssOl08lpBweIQ9SxiOX81vxFu2QFZQNPdKBzDWwsM_WTQduG_3oi4R00sM2fi6ouSPzS7l-Kap94oaIQB8gOvYegFPkjl14Jvz__AnkoNySbhNWDTZLQgpTPMhLuRft3BFav2oIE36R8SanMI_KdkJmczEIc_R7x_YAx_e-oAx6duQLkw2NZ0sczNtZr9wBBy1TpAFDKds8UMC3GMys1hAwC2J9zVh_mCdD-z667_HNcJmNe66yMh4nRNRYZk'
   ),
